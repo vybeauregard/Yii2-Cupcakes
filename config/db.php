8789b424d96d08ba3807c4c48debedbea09692ec
Trey Beauregard <trey@zurka.com>
2015-06-11 09:40:09 -0400

    project initialized for SQLite data storage

--- config/db.php
+++ config/db.php
@@ -2,8 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlite:' . __DIR__ . '/../db/cupcakes.sqlite',
     'charset' => 'utf8',
 ];
---
+++ db/README.md
@@ -0,0 +1,3 @@
+#####This folder is for local storage of the SQLite database file.
+
+Not having a DB server running will simplify the tutorial and development process.
